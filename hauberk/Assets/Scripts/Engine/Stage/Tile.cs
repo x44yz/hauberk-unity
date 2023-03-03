cefd0f4ee96896557d24cf3175b6a45b25c4451c
dongl1n <dongl1n777@qq.com>
2023-03-03 10:47:53 +0800

    update

--- hauberk/Assets/Scripts/Engine/Stage/Tile.cs
+++ hauberk/Assets/Scripts/Engine/Stage/Tile.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -24,29 +25,46 @@ class Motility {
 
     int _bitMask;
 
-    public Motility(_bitMask)
+    public Motility(int _bitMask)
     {
         this._bitMask = _bitMask;
     }
 
     int hashCode => _bitMask;
 
-    bool operator ==(Object other) {
-        if (other is Motility) return _bitMask == other._bitMask;
+    public static bool operator ==(Motility a, object other) {
+        if (other is Motility)
+        {
+            Motility b = other as Motility;
+            return a.hashCode == b.hashCode;
+        }
         return false;
     }
+    public static bool operator !=(Motility a, object other) {
+        return !(a == other);
+    }
+
+    public override bool Equals(object o)  
+    {  
+        return this == o;  
+    }  
+    
+    public override int GetHashCode()  
+    {  
+        return hashCode;  
+    }  
 
     /// Creates a new MotilitySet containing all of the motilities of this and
     /// [other].
-    Motility operator |(Motility other) => Motility._(_bitMask | other._bitMask);
+    public static Motility operator |(Motility a, Motility other) => new Motility(a._bitMask | other._bitMask);
 
     /// Creates a new MotilitySet containing all of the motilities of this
     /// except for the motilities in [other].
-    Motility operator -(Motility other) => Motility._(_bitMask & ~other._bitMask);
+    public static Motility operator -(Motility a, Motility other) => new Motility(a._bitMask & ~other._bitMask);
 
-    bool overlaps(Motility other) => _bitMask & other._bitMask != 0;
+    public bool overlaps(Motility other) => (_bitMask & other._bitMask) != 0;
 
-    string  toString() => _bitMask.toString();
+    string toString() => _bitMask.ToString();
 }
 
 /// Enum-like class for tiles that transport the hero: dungeon entrance, exit,
@@ -63,144 +81,151 @@ class TilePortal {
 }
 
 class TileType {
-  /// The type of a tile when first constructed.
-  public static uninitialized = TileType("uninitialized", null, Motility.none);
+    /// The type of a tile when first constructed.
+    public static TileType uninitialized = new TileType("uninitialized", null, Motility.none);
 
-  public string  name;
+    public string  name;
 
-  /// Where the tile takes the hero, or `null` if it's a regular tile.
-  public TilePortal? portal;
+    /// Where the tile takes the hero, or `null` if it's a regular tile.
+    public TilePortal portal;
 
-  public int emanation;
-  public Object? appearance;
+    public int emanation;
+    public object appearance;
 
-  bool canClose => onClose != null;
+    bool canClose => onClose != null;
 
-  bool canOpen => onOpen != null;
+    bool canOpen => onOpen != null;
 
-  public Motility motility;
+    public Motility motility;
 
-  /// If the tile can be "opened", this is the function that produces an open
-  /// action for it. Otherwise `null`.
-  public Action Function(Vec)? onClose;
+    /// If the tile can be "opened", this is the function that produces an open
+    /// action for it. Otherwise `null`.
+    public Func<Action, Vec> onClose;
 
-  /// If the tile can be "opened", this is the function that produces an open
-  /// action for it. Otherwise `null`.
-  public Action Function(Vec)? onOpen;
+    /// If the tile can be "opened", this is the function that produces an open
+    /// action for it. Otherwise `null`.
+    public Func<Action, Vec> onOpen;
 
-  bool isTraversable => canEnter(Motility.doorAndWalk);
+    public bool isTraversable => canEnter(Motility.doorAndWalk);
 
-  bool isWalkable => canEnter(Motility.walk);
+    public bool isWalkable => canEnter(Motility.walk);
 
-  TileType(this.name, this.appearance, this.motility,
-      {int? emanation, this.portal, this.onClose, this.onOpen})
-      : emanation = emanation ?? 0;
+    TileType(string name, object appearance, Motility motility,
+        int emanation = 0, TilePortal portal = null, 
+        Func<Action, Vec> onClose = null, Func<Action, Vec> onOpen = null)
+    {
+        this.name = name;
+        this.appearance = appearance;
+        this.motility = motility;
+        this.emanation = emanation;
+        this.portal = portal;
+        this.onClose = onClose;
+        this.onOpen = onOpen;
+    }
 
-  /// Whether an actor with [motility] is able to enter this tile.
-  bool canEnter(Motility motility) => this.motility.overlaps(motility);
+    /// Whether an actor with [motility] is able to enter this tile.
+    public bool canEnter(Motility motility) => this.motility.overlaps(motility);
 }
 
 class Tile {
-  /// The tile's basic type.
-  ///
-  /// If you change this during the game, make sure to call
-  /// [Stage.tileOpacityChanged] if the tile's opacity changed.
-  TileType type = TileType.uninitialized;
+    /// The tile's basic type.
+    ///
+    /// If you change this during the game, make sure to call
+    /// [Stage.tileOpacityChanged] if the tile's opacity changed.
+    TileType type = TileType.uninitialized;
 
-  /// Whether some other opaque tile is blocking the hero's view of this tile.
-  ///
-  /// This gets updated by [Fov] as the hero moves around.
-  bool _isOccluded = false;
+    /// Whether some other opaque tile is blocking the hero's view of this tile.
+    ///
+    /// This gets updated by [Fov] as the hero moves around.
+    bool _isOccluded = false;
 
-  bool isOccluded => _isOccluded;
+    bool isOccluded => _isOccluded;
 
-  /// How much visibility is reduced by distance fall-off.
-  int fallOff => _fallOff;
-  int _fallOff = 0;
+    /// How much visibility is reduced by distance fall-off.
+    int fallOff => _fallOff;
+    int _fallOff = 0;
 
-  /// Whether the tile can be seen through or blocks the hero's view beyond it.
-  ///
-  /// We assume any tile that an actor can fly over is also "open" enough to
-  /// be seen through. We don't use [isWalkable] because things like water
-  /// cannot be walked over but can be seen through.
-  bool blocksView => !isFlyable;
+    /// Whether the tile can be seen through or blocks the hero's view beyond it.
+    ///
+    /// We assume any tile that an actor can fly over is also "open" enough to
+    /// be seen through. We don't use [isWalkable] because things like water
+    /// cannot be walked over but can be seen through.
+    bool blocksView => !isFlyable;
 
-  /// Whether the hero can currently see the tile.
-  ///
-  /// To be visible, a tile must not be occluded, in the dark, or too far away.
-  bool isVisible => !isOccluded && illumination > _fallOff;
+    /// Whether the hero can currently see the tile.
+    ///
+    /// To be visible, a tile must not be occluded, in the dark, or too far away.
+    bool isVisible => !isOccluded && illumination > _fallOff;
 
-  /// The total amount of light being cast onto this tile from various sources.
-  ///
-  /// This is a combination of the tile's [emanation], the propagated emanation
-  /// from nearby tiles, light from actors, etc.
-  int illumination => floorIllumination + actorIllumination;
+    /// The total amount of light being cast onto this tile from various sources.
+    ///
+    /// This is a combination of the tile's [emanation], the propagated emanation
+    /// from nearby tiles, light from actors, etc.
+    int illumination => floorIllumination + actorIllumination;
 
-  int floorIllumination = 0;
-  int actorIllumination = 0;
+    int floorIllumination = 0;
+    int actorIllumination = 0;
 
-  /// The amount of light the tile produces.
-  ///
-  /// Includes "native" emanation from the tile itself along with light that
-  /// has been applied to it.
-  int emanation =>
-      (type.emanation + _appliedEmanation).clamp(0, Lighting.floorMax);
+    /// The amount of light the tile produces.
+    ///
+    /// Includes "native" emanation from the tile itself along with light that
+    /// has been applied to it.
+    int emanation =>
+        Mathf.Clamp(type.emanation + _appliedEmanation, 0, Lighting.floorMax);
 
-  /// The extra emanation applied to this tile independent of its type from
-  /// things like light spells.
-  int _appliedEmanation = 0;
+    /// The extra emanation applied to this tile independent of its type from
+    /// things like light spells.
+    int _appliedEmanation = 0;
 
-  /// If you call this, make sure to call [Stage.tileEmanationChanged()].
-  void addEmanation(int offset) {
-    _appliedEmanation =
-        (_appliedEmanation + offset).clamp(0, Lighting.floorMax);
-  }
-
-  void maxEmanation(int amount) {
-    _appliedEmanation = math.max(_appliedEmanation, amount);
-  }
-
-  bool _isExplored = false;
-
-  bool isExplored => _isExplored;
-
-  /// Marks this tile as explored if the hero can see it and hasn't previously
-  /// explored it.
-  ///
-  /// This should not be called directly. Instead, call [Stage.explore()].
-  ///
-  /// Returns true if this tile was explored just now.
-  bool updateExplored({bool? force}) {
-    force ??= false;
-    if ((force || isVisible) && !_isExplored) {
-      _isExplored = true;
-      return true;
+    /// If you call this, make sure to call [Stage.tileEmanationChanged()].
+    void addEmanation(int offset) {
+        _appliedEmanation =
+            Mathf.Clamp(_appliedEmanation + offset, 0, Lighting.floorMax);
     }
 
-    return false;
-  }
+    void maxEmanation(int amount) {
+        _appliedEmanation = Mathf.Max(_appliedEmanation, amount);
+    }
 
-  void updateVisibility(bool isOccluded, int fallOff) {
-    _isOccluded = isOccluded;
-    _fallOff = fallOff;
-  }
+    bool _isExplored = false;
 
-  /// The element of the substance occupying this file: fire, water, poisonous
-  /// gas, etc.
-  Element element = Element.none;
+    bool isExplored => _isExplored;
 
-  /// How much of [element] is occupying the tile.
-  int substance = 0;
+    /// Marks this tile as explored if the hero can see it and hasn't previously
+    /// explored it.
+    ///
+    /// This should not be called directly. Instead, call [Stage.explore()].
+    ///
+    /// Returns true if this tile was explored just now.
+    bool updateExplored(bool force = false) {
+        if ((force || isVisible) && !_isExplored) {
+            _isExplored = true;
+            return true;
+        }
+        return false;
+    }
 
-  bool isWalkable => type.isWalkable;
+    void updateVisibility(bool isOccluded, int fallOff) {
+        _isOccluded = isOccluded;
+        _fallOff = fallOff;
+    }
 
-  bool isTraversable => type.isTraversable;
+    /// The element of the substance occupying this file: fire, water, poisonous
+    /// gas, etc.
+    Element element = Element.none;
 
-  bool isFlyable => canEnter(Motility.fly);
+    /// How much of [element] is occupying the tile.
+    int substance = 0;
 
-  bool isClosedDoor => type.motility == Motility.door;
+    bool isWalkable => type.isWalkable;
 
-  TilePortal? portal => type.portal;
+    bool isTraversable => type.isTraversable;
 
-  bool canEnter(Motility motility) => type.canEnter(motility);
+    bool isFlyable => canEnter(Motility.fly);
+
+    bool isClosedDoor => type.motility == Motility.door;
+
+    TilePortal? portal => type.portal;
+
+    bool canEnter(Motility motility) => type.canEnter(motility);
 }
